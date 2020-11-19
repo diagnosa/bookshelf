8b99223000632cf6018e3ce119a66059a2dfd12b
diagnosa <d.fenomena@live.com>
2020-11-19 20:57:13 +0700

    fix placeholder

--- src/component/LogoImage/index.js
+++ src/component/LogoImage/index.js
@@ -1,9 +1,13 @@
+import { useImage } from "react-image";
 import logo from './comingSoonLogo.png';
 import './LogoImage.css';
 
 function LogoImage() {
+  const {src} = useImage({
+    srcList: logo,
+  })
   return (
-    <img src={logo} className="Logo-image" alt="logo" />
+    <img src={src} className="Logo-image" alt="logo" />
   );
 }
 
