00a8a81d3b4ad32b9e142d1420c06349d015cb84
diagnosa <d.fenomena@live.com>
2020-12-16 11:57:58 +0700

    add simple cards to display booklist

--- src/component/Catalog/index.js
+++ src/component/Catalog/index.js
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
 import axios from 'axios';
 import { parseStringPromise } from "xml2js";
+import Card from '../Card';
 
+const url = `${process.env.REACT_APP_GR_URL}/review/list/${process.env.REACT_APP_GR_UID}.xml`;
 function Catalog() { 
     const [error, setError] = useState(null);
     const [isLoaded, setIsLoaded] = useState(false);
@@ -9,48 +11,52 @@ function Catalog() {
   
     useEffect(() => {
         axios({
-            url: `${process.env.REACT_APP_GR_URL}shelf/list.xml`,
+            url,
             headers: {
-                'Accept': 'application/xml',
+                Accept: 'application/xml',
             },
             params: {
-                key: process.env.REACT_APP_GR_API_KEY, 
-                user_id: process.env.REACT_APP_GR_UID, 
+                key: process.env.REACT_APP_GR_API_KEY,
+                v: 2,
+                sort: 'num_ratings',
             },
         })
-        .then(result=>result.data)
-        .then(parseStringPromise)
-        .then(
-            (result) => {
-                const {
-                    GoodreadsResponse:{
-                        shelves
-                }} = result;
-                setIsLoaded(true);
-                setItems(shelves[0].user_shelf);
-            },
-            (error) => {
-                setIsLoaded(true);
-                setError(error);
-            }
-        )
+            .then((result) => result.data)
+            .then(parseStringPromise)
+            .then(
+                (result) => {
+                    const {
+                        GoodreadsResponse: { reviews },
+                    } = result;
+                    setIsLoaded(true);
+                    console.log(reviews[0].review);
+                    setItems(reviews[0].review);
+                },
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            );
     }, [])
   
-    if (error) {
+    if (error){
         return <div>Error: {error.message} {error.response}</div>;
-    } else if (!isLoaded) {
-        return <div>Loading...</div>;
-    } else {
-        return (
-            <ul>
-                {items.map(item => (
-                    <li key={item.id[0]._}>
-                        {item.name[0]}
-                    </li>
-                ))}
-            </ul>
-        );
     }
+    if (!isLoaded){
+        return <div>Loading...</div>;
+    }
+        
+    return (
+        <div>
+            {items.map((item) => (
+                <Card
+                    key={item.id[0]}
+                    title={item.book[0].title[0] || 'judul'}
+                    image={item.book[0].image_url[0] || 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1467556925l/30835803._SX98_.jpg'}
+                />
+            ))}
+        </div>
+    );
   }
 
 export default Catalog;
